4885c88896444c19bab8c6586008fb2b141ddbb8
Sam Schmidt <samuel@dersam.net>
2016-04-01 11:02:35 -0400

    Allow configuration of frameworks, move validation to framework level.

---
+++ framework.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ *
+ * @author Sam Schmidt <samuel@dersam.net>
+ * @since 2016-04-01
+ */
+
+return [
+    'name' => 'behat',
+    'validation' =>[
+        'files' =>[
+            'behat.yml'
+        ]
+    ]
+];
