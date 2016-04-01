710ceaa1ab583cbb3ca412b101198dc3f14b7164
Sam Schmidt <samuel@dersam.net>
2016-04-01 11:44:42 -0400

    Initialize module for testing with init by framework.
    Resolves #21.

--- framework.php
+++ framework.php
@@ -6,11 +6,49 @@
  * @since 2016-04-01
  */
 
+$yml = <<<YML
+# behat.yml
+
+default:
+    autoload:
+        '': %paths.base%/tests/behat/contexts
+    suites:
+        default:
+          paths: [ %paths.base%/tests/behat ]
+          contexts:
+            - FeatureContext
+    extensions:
+        Behat\MinkExtension:
+            base_url: https://develop.vagrant.dev
+            browser_name: 'chrome'
+            selenium2:
+              wd_host: "http://selenium.dev:4444/wd/hub"
+
+firefox:
+    extensions:
+        Behat\MinkExtension:
+          browser_name: 'firefox'
+
+chrome:
+    extensions:
+        Behat\MinkExtension:
+          browser_name: 'chrome'
+
+YML;
+
 return [
     'name' => 'behat',
     'validation' =>[
         'files' =>[
             'behat.yml'
+        ]
+    ],
+    'init' => [
+        'dirs' => [
+            './tests/behat'
+        ],
+        'files' => [
+            'behat.yml' => $yml
         ]
     ]
 ];
